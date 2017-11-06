65777aad34020ea3b20d0e5bc90ed7752eb0d0cc
eszael <33296356+eszael@users.noreply.github.com>
2017-11-06 11:15:03 +0100

    v4
    output OK

--- src/onePackage/Sample.java
+++ src/onePackage/Sample.java
@@ -26,47 +26,82 @@ public class Sample {
 				
 		////////////////////////////////////////////////
 		
-		driver.findElement(By.xpath("//*[@id=\"navigationmenu\"]/div[2]/ul/li[2]/a")).click();
+		String my_Title00 = driver.getTitle();
+		System.out.print("<title>" + my_Title00);System.out.println("<title>");
+		String my_Url00 = driver.getCurrentUrl();
+		System.out.print("	<url>" + my_Url00);System.out.println("<url>");
 		
-		List<WebElement> childLinks1 = driver.findElement(By.xpath("//*[@id=\"ttr_content\"]")).findElements(By.tagName("a"));
-		for(WebElement Link : childLinks1) {
-				System.out.println(Link.getText());
+		
+		driver.findElement(By.xpath("//*[@id=\"navigationmenu\"]/div[2]/ul/li[2]/a")).click();
+		String my_Title01 = driver.getTitle();
+		System.out.print("<title>" + my_Title01);System.out.println("<title>");
+		String my_Url01 = driver.getCurrentUrl();
+		System.out.print("	<url>" + my_Url01);System.out.println("<url>");
+		
+		List<WebElement> childLinks01 = driver.findElement(By.xpath("//*[@id=\"ttr_content\"]")).findElements(By.tagName("a"));
+		for(WebElement Link : childLinks01) {
+			System.out.print("		<title>" + Link.getText());System.out.println("<title>");
+			System.out.print("			<url>" + Link.getAttribute("href"));System.out.println("<url>");
 		}
 		
 		////////////////////////////////////////////////
 		
 		driver.findElement(By.xpath("//*[@id=\"navigationmenu\"]/div[2]/ul/li[3]/a")).click();
-
-		List<WebElement> childLinks2 = driver.findElement(By.xpath("//*[@id=\"ttr_content\"]")).findElements(By.tagName("a"));
-		for(WebElement Link : childLinks2) {
-				System.out.println(Link.getText());
+		String my_Title02 = driver.getTitle();
+		System.out.print("<title>" + my_Title02);System.out.println("<title>");
+		String my_Url02 = driver.getCurrentUrl();
+		System.out.print("	<url>" + my_Url02);System.out.println("<url>");
+		
+		
+		List<WebElement> childLinks02 = driver.findElement(By.xpath("//*[@id=\"ttr_content\"]")).findElements(By.tagName("a"));
+		for(WebElement Link : childLinks02) {
+			System.out.print("		<title>" + Link.getText());System.out.println("<title>");
+			System.out.print("			<url>" + Link.getAttribute("href"));System.out.println("<url>");
 		}
 		
 		////////////////////////////////////////////////		
 		
 		driver.findElement(By.xpath("//*[@id=\"navigationmenu\"]/div[2]/ul/li[4]/a")).click();
-
-		List<WebElement> childLinks3 = driver.findElement(By.xpath("//*[@id=\"ttr_content\"]")).findElements(By.tagName("a"));
-		for(WebElement Link : childLinks3) {
-				System.out.println(Link.getText());
+		String my_Title03 = driver.getTitle();
+		System.out.print("<title>" + my_Title03);System.out.println("<title>");
+		String my_Url03 = driver.getCurrentUrl();
+		System.out.print("	<url>" + my_Url03);System.out.println("<url>");
+		
+		
+		List<WebElement> childLinks03 = driver.findElement(By.xpath("//*[@id=\"ttr_content\"]")).findElements(By.tagName("a"));
+		for(WebElement Link : childLinks03) {
+			System.out.print("		<title>" + Link.getText());System.out.println("<title>");
+			System.out.print("			<url>" + Link.getAttribute("href"));System.out.println("<url>");
 		}
 		
 		////////////////////////////////////////////////
 		
 		driver.findElement(By.xpath("//*[@id=\"navigationmenu\"]/div[2]/ul/li[5]/a")).click();
-
-		List<WebElement> childLinks4 = driver.findElement(By.xpath("//*[@id=\"ttr_content\"]")).findElements(By.tagName("a"));
-		for(WebElement Link : childLinks4) {
-				System.out.println(Link.getText());
+		String my_Title04 = driver.getTitle();
+		System.out.print("<title>" + my_Title04);System.out.println("<title>");
+		String my_Url04 = driver.getCurrentUrl();
+		System.out.print("	<url>" + my_Url04);System.out.println("<url>");
+		
+		
+		List<WebElement> childLinks04 = driver.findElement(By.xpath("//*[@id=\"ttr_content\"]")).findElements(By.tagName("a"));
+		for(WebElement Link : childLinks04) {
+			System.out.print("		<title>" + Link.getText());System.out.println("<title>");
+			System.out.print("			<url>" + Link.getAttribute("href"));System.out.println("<url>");
 		}
 		
 		////////////////////////////////////////////////
 		
 		driver.findElement(By.xpath("//*[@id=\"navigationmenu\"]/div[2]/ul/li[6]/a")).click();
-
-		List<WebElement> childLinks5 = driver.findElement(By.xpath("//*[@id=\"ttr_content\"]")).findElements(By.tagName("a"));
-		for(WebElement Link : childLinks5) {
-				System.out.println(Link.getText());
+		String my_Title05 = driver.getTitle();
+		System.out.print("<title>" + my_Title05);System.out.println("<title>");
+		String my_Url05 = driver.getCurrentUrl();
+		System.out.print("	<url>" + my_Url05);System.out.println("<url>");
+		
+		
+		List<WebElement> childLinks05 = driver.findElement(By.xpath("//*[@id=\"ttr_content\"]")).findElements(By.tagName("a"));
+		for(WebElement Link : childLinks05) {
+			System.out.print("		<title>" + Link.getText());System.out.println("<title>");
+			System.out.print("			<url>" + Link.getAttribute("href"));System.out.println("<url>");
 		}
 		
 		////////////////////////////////////////////////
